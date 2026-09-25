9c9dc550141ffd7c3b2e740359a81203138fcb67
harvie <leyharviep@gmail.com>
2026-09-25 14:10:30 +0800

    Remove BOM from functions wrapper

--- includes/functions_fixed.php
+++ includes/functions_fixed.php
@@ -1,6 +1,3 @@
-﻿<?php
-/**
- * functions_fixed.php
- * Wrapper that includes the main functions.php file
- * Ensures all helper functions are available without duplication
- */require_once __DIR__ .'/functions.php';
+<?php
+
+require_once __DIR__ . '/functions.php';
